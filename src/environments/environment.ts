ad209e2c2f1857cda12d19b042d21efd832b581f
paris <aparecidapiresbarb@gmail.com>
2018-05-14 18:27:44 -0300

    Add firebase api keys, material design + create core/shared/material modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // here goes the information of your application created in Firebase
+  // https://firebase.google.com/
+  firebase: {
+    apiKey: "yourApiKey",
+    authDomain: "yourApplication.firebaseapp.com",
+    databaseURL: "https://yourApplication.firebaseio.com",
+    projectId: "yourApplication",
+    storageBucket: "yourApplication.appspot.com",
+    messagingSenderId: "youMessagingSenderId"
+  }
 };
