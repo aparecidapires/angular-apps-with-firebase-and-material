652d6a07287d0dcd0a9f4a73b4a800cc237c30b1
paris <aparecidapiresbarb@gmail.com>
2018-05-20 17:52:26 -0300

    Create post module and it's components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
 
-
 import { AppComponent } from './app.component';
-import { MaterialModule } from './/material.module';
 import { CoreModule } from './core/core.module';
 import { RoutingModule } from './/routing.module';
 import { environment } from '../environments/environment';
+import { PostModule } from './post/post.module';
 
 
 @NgModule({
@@ -17,10 +17,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     CoreModule,
-    RoutingModule
+    RoutingModule,
+    PostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
